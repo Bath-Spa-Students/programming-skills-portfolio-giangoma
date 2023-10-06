1557044e3787f193ad71f04315c96ba2521ca39e
giangoma <144329931+giangoma@users.noreply.github.com>
2023-10-06 19:01:36 +0400

    Revert "exercise4"
    This reverts commit ce4db40145625d4ab58547e29cc3b573e125d28b.

--- Chapter 1- Getting Started with Python/Exercises/practice1.py
+++
@@ -1,9 +0,0 @@
-# Write a program to output the letters W,X,Y,Z, each on a seperate line
-
-print ("w")
-print ("x")
-print ("y")
-print ("z")
-
-'''age = input ("Enter your age: )
-print (age) '''
--- Chapter 1- Getting Started with Python/Exercises/practice2.py
+++
@@ -1,4 +0,0 @@
-# Create a program to output "hi" 50 times, without any seperator
-
-w = ("hi ")
-print (w * 50)
