9ab01bd5b9c7f1b17612f85c855955c1e46a9ebb
giangoma <144329931+giangoma@users.noreply.github.com>
2023-10-12 13:29:26 +0400

    Name.py

---
+++ Chapter 1- Getting Started with Python/Exercises/Untitled.py
@@ -0,0 +1,28 @@
+f1 = "Joseph"
+f2 = " Gianne"
+name = (f1+f2)
+print (name, end = "")
+print (" ahahahhaha")
+
+print("Bath\tSpa")
+print("My name is\nGianne")
+
+
+print("kokokokok\tkokokokko")
+
+print("Hehehehe\tHohohoho\nHahahaha\tHuhuhuhuhu")
+print("HELLO")
+
+val = 13
+print(f'The radius of the circle is {val + 8}.')
+
+
+import turtle
+turtle.left(100)
+turtle.dot()
+turtle.back(100)
+turtle.dot()
+turtle.right(100)
+turtle.dot()
+turtle.forward(100)
+turtle.dot()
