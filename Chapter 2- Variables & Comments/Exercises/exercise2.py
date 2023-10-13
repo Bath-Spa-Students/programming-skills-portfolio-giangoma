2f2b9a54f425740292ae7d6af016ce0740b22785
giangoma <144329931+giangoma@users.noreply.github.com>
2023-10-13 18:40:55 +0400

    exercise2.py

---
+++ Chapter 2- Variables & Comments/Exercises/exercise2.py
@@ -0,0 +1,2 @@
+lequote = ("You can't be afraid to fail. It's the only way you succeed. You're not gonna succeed all the time and I know that.")
+print (f'LeBron James, a famous basketball star had once said, "{lequote}"')
