c9dfa83db2b2095a3e343603ba669936ffa8b27e
giangoma <144329931+giangoma@users.noreply.github.com>
2023-10-22 20:37:10 +0400

    exercise2.py

---
+++ Chapter 3- Data Structures/Exercises/exercise2.py
@@ -0,0 +1,10 @@
+names = ['Anne', 'Aviona', 'Danie', 'JL']
+
+stmt1 = f"Good day, {names[0]}!"
+print(stmt1)
+stmt2 = f"Good day, {names[1]}!"
+print(stmt2)
+stmt3 = f"Good day, {names[2]}!"
+print(stmt3)
+stmt4 = f"Good day, {names[3]}!"
+print(stmt4)
