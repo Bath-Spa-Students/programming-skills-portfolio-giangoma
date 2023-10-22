18d7eb8e75f1373fdbd453612ef621d20c1413f2
giangoma <144329931+giangoma@users.noreply.github.com>
2023-10-22 20:37:24 +0400

    exercise4.py

---
+++ Chapter 3- Data Structures/Exercises/exercise4.py
@@ -0,0 +1,8 @@
+names = ['James Murray', 'Sal Vulcano', 'Brian Quinn']
+
+stmt1 = f"Greetings, {names[0]}, of the Impractical Jokers! I would like to formally invite you to a dinner feast."
+stmt2 = f"Greetings, {names[1]}, of the Impractical Jokers! I would like to formally invite you to a dinner feast."
+stmt3 = f"Greetings, {names[2]}, of the Impractical Jokers! I would like to formally invite you to a dinner feast."
+print(stmt1)
+print(stmt2)
+print(stmt3)
