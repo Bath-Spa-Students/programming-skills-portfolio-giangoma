9f1ae77d4a20e210893429ebb4f9b8c500eaaba1
giangoma <144329931+giangoma@users.noreply.github.com>
2023-10-29 14:31:00 +0400

    exercise7.py

---
+++ Chapter 3- Data Structures/Exercises/exercise7.py
@@ -0,0 +1,39 @@
+#dream destinations
+
+destinations = ['South Korea', 'Japan', 'France', 'Italy', 'Canada']
+
+#Original Order
+print(f"\nOriginal Order: {destinations}")
+
+#Alphabetically
+print(f"\nAlphabetical: {sorted(destinations)} ")
+
+#Original Order
+print(f"\nOriginal Order: {destinations}")
+
+#Reverse Alphabetical
+sorted_destinations = sorted(destinations, reverse=True)
+print(f"\nReverse Alphabetical: {sorted_destinations}")
+
+#Original Order
+print(f"\nOriginal Order: {destinations}")
+
+#Reversed Order
+destinations.reverse()
+print(f"\nReversed: {destinations}")
+
+#Original Order
+destinations.reverse()
+print(f"\nReversed to Original: {destinations}")
+
+#Alphabetical
+destinations.sort()
+print(f"\nAlphabetical: {destinations}")
+
+#Reverse ALphabetical
+destinations.sort(reverse=True)
+print(f"\nReverse Alphabetical: {destinations}")
+
+
+
+
