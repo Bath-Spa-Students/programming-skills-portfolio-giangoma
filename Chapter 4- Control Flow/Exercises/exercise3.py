d6c23f5f41989ad6dd58a47e003453cb83e048a3
giangoma <144329931+giangoma@users.noreply.github.com>
2023-10-29 18:25:42 +0400

    exercise3.py

---
+++ Chapter 4- Control Flow/Exercises/exercise3.py
@@ -0,0 +1,26 @@
+#IF BLOCK
+alien_color = 'green'
+if alien_color == 'green':
+    print("You have just earned yourself 5 points!")
+elif alien_color == 'yellow':
+    print("You have just earned yourself 10 points!")
+else:
+    print("You have just earned yourself 15 points!")
+
+#ELIF BLOCK
+alien_color = 'yellow'
+if alien_color == 'green':
+    print("You have just earned yourself 5 points!")
+elif alien_color == 'yellow':
+    print("You have just earned yourself 10 points!")
+else:
+    print("You have just earned yourself 15 points!")
+
+#ELSE BLOCK
+alien_color = 'red'
+if alien_color == 'yellow':
+    print("You have just earned yourself 5 points!")
+elif alien_color == 'green':
+    print("You have just earned yourself 10 points!")
+else:
+    print("You have just earned yourself 15 points!")
