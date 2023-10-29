6357481b81007d06cf8956d275d68bcc42ce1cd7
giangoma <144329931+giangoma@users.noreply.github.com>
2023-10-29 18:25:37 +0400

    exercise2.py

---
+++ Chapter 4- Control Flow/Exercises/exercise2.py
@@ -0,0 +1,17 @@
+#IF BLOCK
+alien_color = 'green'
+if alien_color == 'green':
+    print("You just shot the alien and gained 5 points!")
+else:
+    print("You just gained 10 points. Congratulations!")
+
+#ELSE BLOCK
+alien_color = 'yellow'
+if alien_color == 'red':
+    print("You just shot the alien and gained 5 points!")
+else:
+    print("You just gained 10 points. Congratulations!")
+
+    
+
+
