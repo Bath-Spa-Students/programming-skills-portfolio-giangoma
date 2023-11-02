edcd4630d4f96d40f587019adff4a461bf28a148
giangoma <144329931+giangoma@users.noreply.github.com>
2023-11-02 12:43:56 +0400

    exercise2.py

---
+++ Chapter 5- Dictionaries/Exercises/exercise2.py
@@ -0,0 +1,14 @@
+#Think of five programming words you’ve learned about in the previous chapters. Use these words as the keys in your glossary, and store 
+#their meanings as values.
+#Print each word and its meaning as neatly formatted output.
+
+glossary = {'Python': 'a high-level programming language',
+            'Integer':'Any whole number',
+            'Float':'Any decimal number',
+            'String':'Anything that is text',
+            'List':'Something that stores values'}
+print("Python: " + glossary["Python"])
+print("Integer: " + glossary["Integer"])
+print("Float: " + glossary["Float"])
+print("String: " + glossary["String"])
+print("List: " + glossary["List"])
