1e63e50425375391188afd0ebe9cd2cff8e45fb5
giangoma <144329931+giangoma@users.noreply.github.com>
2023-11-02 12:44:04 +0400

    exercise3.py

---
+++ Chapter 5- Dictionaries/Exercises/exercise3.py
@@ -0,0 +1,12 @@
+glossary = {'Python': 'a high-level programming language',
+            'Integer':'Any whole number',
+            'Float':'Any decimal number',
+            'String':'Anything that is text',
+            'List':'Something that stores values',
+            'Boolean':'True or False',
+            'Dictionary':'Stores key-value pairs',
+            'Keywords':'Defines the syntax in Python',
+            'Pseudocode':'a rough draft/code of a program',
+            'Flowchart':'Visual representation of code'}
+for key, value in glossary.items():
+    print(key, ":", value)
