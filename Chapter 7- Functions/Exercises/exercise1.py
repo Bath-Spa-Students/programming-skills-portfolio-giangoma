cda8907fb208575a71cd881fe1e56832923c7813
giangoma <144329931+giangoma@users.noreply.github.com>
2023-11-13 09:49:29 +0400

    exercise1.py

---
+++ Chapter 7- Functions/Exercises/exercise1.py
@@ -0,0 +1,7 @@
+#Write a function called display_message() that prints one sentence telling everyone what you are learning about in this chapter. 
+#Call the function, and make sure the message displays correctly.
+
+def display_message():
+    message = ("In this chapter, I will be learning about Functions and their uses in Python.")
+    print(message)
+display_message()
